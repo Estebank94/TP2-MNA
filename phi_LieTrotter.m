45e718a7d71ccd1a79b25355d48521e7e6377789
Esteban Kramer <estebankramer@gmail.com>
2019-11-17 14:41:06 -0300

    phi lie trotter

---
+++ phi_LieTrotter.m
@@ -0,0 +1,17 @@
+function v = LieTrotterL(dt, v, x)
+  v = Lineal(dt, v, x);
+  v = NonLineal(dt, v, x);
+end
+
+function v = LieTrotterNL(dt, v, x)
+  v = NonLineal(dt, v, x);
+  v = Lineal(dt, v, x);
+end
+
+function U = Lineal(delta_t, U, k)
+  U = U.*exp(1i*k.^3*delta_t);
+end
+
+function U = NonLineal(delta_t, U, k)
+  U = U.*exp(3i*k.^3*delta_t); % ????????
+end
